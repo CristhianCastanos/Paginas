f330c37fcbae0aa9869270799fb1af219c8b5b20
CristhianCastanos <cristhian.castanos@sparktechs.com>
2022-10-25 16:26:47 -0700

    leccion-2

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//int numeros enteros
+//float
+//double
+//char
+//string
+//object
+
+
+//------variables en js
+//const
+//let -su valor puede cambiar 
+//var -su valor puede cambiar 
+
+const mensajeBienvenida="Hola bienvenido";
+
+var a,b;
+a=3;
+b=5;
+let suma=a+b;
+
+//API
+//Se utiliza cuando se desconoce el sitio que se obtiene
+var rspuesta=app.get(x=>"https://sitio.com/envioCosas")
